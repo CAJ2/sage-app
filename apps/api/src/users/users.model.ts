ef0c3fdf24e408cfd7cc7a1a7de580183d58feac
Christian Johansen <johansen.christian.a@gmail.com>
2025-02-12 14:46:46 +0100

    Updates

--- apps/api/src/users/users.model.ts
+++ apps/api/src/users/users.model.ts
@@ -16,6 +16,39 @@ export class User extends CreatedUpdated {
 
   @Field(() => [Identity])
   identities: Identity[];
+
+  @Field({ nullable: true })
+  email?: string;
+
+  @Field({ nullable: true })
+  email_verified?: boolean;
+
+  @Field({ nullable: true })
+  username?: string;
+
+  @Field({ nullable: true })
+  avatar_url?: string;
+
+  @Field({ nullable: true })
+  last_ip?: string;
+
+  @Field({ nullable: true })
+  last_login?: Date;
+
+  @Field({ nullable: true })
+  login_count?: number;
+
+  @Field({ nullable: true })
+  last_password_reset?: Date;
+
+  @Field({ nullable: true })
+  blocked?: boolean;
+
+  @Field(() => [String], { nullable: true })
+  blocked_for?: string[];
+
+  @Field({ nullable: true })
+  profile?: any;
 }
 
 @ObjectType()
