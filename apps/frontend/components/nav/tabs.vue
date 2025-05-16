03ecb5a6b1dca269061d34aa05bf052a3f862eac
Christian Johansen <johansen.christian.a@gmail.com>
2025-05-16 21:25:31 +0200

    feat(nav): Proper navbar now

--- apps/frontend/components/nav/tabs.vue
+++ apps/frontend/components/nav/tabs.vue
@@ -1,60 +1,60 @@
 <template>
-  <div class="flex items-center justify-around bg-base-200 p-3 relative">
+  <div
+    class="flex justify-around justify-items-center items-center bg-base-200 relative"
+  >
     <!-- Left Tabs -->
-    <div class="space-x-4">
-      <button
-        v-for="(tab, index) in leftTabs"
-        :key="index"
-        class="flex-1 items-center p-1"
-        :class="{
-          'text-accent border-b-2 border-primary': activeTab === tab.path,
-          'text-base-content': activeTab !== tab.path,
-        }"
-      >
-        <router-link
-          :to="{ path: tab.path }"
-          class="flex flex-col items-center"
+    <div
+      v-for="(tab, index) in leftTabs"
+      :key="index"
+      class="flex-1 flex justify-center"
+    >
+      <router-link :to="{ path: tab.path }">
+        <button
+          class="flex flex-col items-center p-1 cursor-pointer"
+          :class="{
+            'text-accent': activeTab === tab.path,
+            'text-base-content': activeTab !== tab.path,
+          }"
         >
           <font-awesome-icon :icon="tab.icon" class="w-6 h-6" />
           <span class="text-sm">{{ tab.label }}</span>
-        </router-link>
-      </button>
+        </button>
+      </router-link>
     </div>
 
     <!-- Center Search Button -->
-    <div class="absolute inset-x-0 top-0 flex justify-center -translate-y-1/3">
-      <button class="w-16 h-16 btn btn-primary rounded-full">
-        <router-link
-          :to="{ path: '/search' }"
-          class="flex items-center justify-center"
-        >
+    <div class="inset-x-0 top-0 -translate-y-1/3 p-3">
+      <router-link
+        :to="{ path: '/search' }"
+        class="flex items-center justify-center"
+      >
+        <button class="w-16 h-16 btn btn-primary rounded-full p-0">
           <font-awesome-icon
             icon="fa-solid fa-magnifying-glass"
             class="w-8 h-8"
           />
-        </router-link>
-      </button>
+        </button>
+      </router-link>
     </div>
 
     <!-- Right Tabs -->
-    <div class="align-end space-x-4">
-      <button
-        v-for="(tab, index) in rightTabs"
-        :key="index"
-        class="flex-1 items-center p-y-1"
-        :class="{
-          'text-primary border-b-2 border-primary': activeTab === tab.path,
-          'text-base-content': activeTab !== tab.path,
-        }"
-      >
-        <router-link
-          :to="{ path: tab.path }"
-          class="flex flex-col items-center"
+    <div
+      v-for="(tab, index) in rightTabs"
+      :key="index"
+      class="flex-1 flex justify-center"
+    >
+      <router-link :to="{ path: tab.path }">
+        <button
+          class="flex flex-col items-center p-1 cursor-pointer"
+          :class="{
+            'text-accent': activeTab === tab.path,
+            'text-base-content': activeTab !== tab.path,
+          }"
         >
           <font-awesome-icon :icon="tab.icon" class="w-6 h-6" />
           <span class="text-sm">{{ tab.label }}</span>
-        </router-link>
-      </button>
+        </button>
+      </router-link>
     </div>
   </div>
 </template>
