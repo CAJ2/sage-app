75c65c1dca987e8cf01f6ef2ed287babb6b1ee8f
Christian Johansen <johansen.christian.a@gmail.com>
2025-05-31 22:25:36 +0200

    feat(ui,places): Places page zooms to selected region

--- apps/frontend/pages/places/index.vue
+++ apps/frontend/pages/places/index.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
+    <div
+      class="fixed top-0 z-10 w-[calc(100vw-70px)] max-w-2xl bg-white shadow-md text-black m-4 rounded-full focus-visible:outline-hidden flex"
+    >
+      <span class="flex items-center justify-center px-2">
+        <font-awesome-icon
+          icon="fa-solid fa-magnifying-glass"
+          class="text-neutral-700 mx-2"
+        ></font-awesome-icon>
+      </span>
+      <input
+        id="search"
+        v-model="searchInput"
+        type="text"
+        placeholder="Search..."
+        class="w-full p-2"
+      />
+    </div>
     <div class="map-wrap">
       <div ref="mapContainer" class="map"></div>
     </div>
@@ -7,9 +24,40 @@
 </template>
 
 <script setup lang="ts">
-import maplibregl, { Map, NavigationControl } from 'maplibre-gl'
+import maplibregl, {
+  Map,
+  NavigationControl,
+  type LngLatLike,
+} from 'maplibre-gl'
 import { Protocol } from 'pmtiles'
 import type { ShallowRef } from 'vue'
+
+const searchInput = ref('')
+
+const regionStore = useRegionStore()
+const regionQuery = gql`
+  query RegionQuery($id: ID!) {
+    getRegion(id: $id) {
+      id
+      name
+      placetype
+      bbox
+      min_zoom
+    }
+  }
+`
+type RegionResult = {
+  getRegion: {
+    id: string
+    name?: string
+    placetype: string
+    bbox?: [number, number, number, number]
+    min_zoom?: number
+  }
+}
+const { data: regionData } = await useAsyncQuery<RegionResult>(regionQuery, {
+  id: regionStore.selectedRegion,
+})
 
 const mapContainer = shallowRef(null)
 const map: ShallowRef<Map | null> = shallowRef(null)
@@ -21,11 +69,22 @@ onMounted(() => {
   if (!mapContainer.value) {
     return
   }
+  const center: LngLatLike = regionData.value?.getRegion?.bbox
+    ? [
+        (regionData.value.getRegion.bbox[0] +
+          regionData.value.getRegion.bbox[2]) /
+          2,
+        (regionData.value.getRegion.bbox[1] +
+          regionData.value.getRegion.bbox[3]) /
+          2,
+      ]
+    : [0, 0]
+  console.log('Map Center:', center)
   map.value = new Map({
     container: mapContainer.value,
     style: 'map-style-light.json',
-    center: [0, 0],
-    zoom: 1,
+    center,
+    zoom: regionData.value?.getRegion?.min_zoom || 2,
   })
   map.value.addControl(new NavigationControl(), 'top-right')
 })
