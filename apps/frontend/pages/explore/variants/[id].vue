6aa5d50327bc63326812207266974b4ec9517517
Christian Johansen <johansen.christian.a@gmail.com>
2025-05-31 22:28:22 +0200

    feat(ui,variants): Initial attempt at variants recycling info

--- apps/frontend/pages/explore/variants/[id].vue
+++ apps/frontend/pages/explore/variants/[id].vue
@@ -7,42 +7,95 @@
       :image="data?.getVariant.image_url"
       back="true"
     ></NavTopbar>
-    <div class="p-3">
+    <div class="p-3 pb-4">
       <ScoreBar :score="40"></ScoreBar>
     </div>
-    <ul class="list bg-base-100 rounded-box shadow-md">
-      <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Components</li>
-      <li v-if="status === 'pending'" class="list-row">
-        <div class="skeleton h-4 w-28"></div>
-        <div class="skeleton h-4 w-full"></div>
-        <div class="skeleton h-4 w-full"></div>
-      </li>
-
-      <div v-if="data">
-        <li
-          v-for="component in data.getVariant.components.nodes"
-          :key="component.id"
-          class="list-row"
+    <Collapsible v-model:open="recyclingOpen" class="space-y-2">
+      <CollapsibleTrigger as-child>
+        <div
+          class="flex items-center justify-between space-x-4 p-4 border-t-1 border-b-1 border-neutral-700"
         >
-          <div>
-            <img class="size-10 rounded-box" :src="component.image_url" />
-          </div>
-          <div>
-            <div class="text-bold">{{ component.name }}</div>
-            <div class="text-xs opacity-70">
-              {{ component.desc }}
-            </div>
-          </div>
-        </li>
-      </div>
+          <h4 class="text-md font-semibold">Recycle</h4>
+          <Button variant="ghost" size="sm" class="w-9 p-0">
+            <font-awesome-icon
+              icon="fa-solid fa-chevron-down"
+              class="w-4 h-4 transition-transform"
+              :class="{ 'rotate-180': recyclingOpen }"
+            />
+            <span class="sr-only">Toggle</span>
+          </Button>
+        </div>
+      </CollapsibleTrigger>
+      <CollapsibleContent class="space-y-2">
+        <ul class="list bg-base-100 rounded-box shadow-md">
+          <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Components</li>
+          <li v-if="status === 'pending'" class="list-row">
+            <div class="skeleton h-4 w-28"></div>
+            <div class="skeleton h-4 w-full"></div>
+            <div class="skeleton h-4 w-full"></div>
+          </li>
 
-      <li v-else>There are no components to show</li>
-    </ul>
+          <div v-if="recyclingResult">
+            <div
+              v-for="component in recyclingResult.getVariant.components.nodes"
+              :key="component.id"
+            >
+              <li class="list-row">
+                <div>
+                  <img class="size-10 rounded-box" :src="component.image_url" />
+                </div>
+                <div>
+                  <div class="text-bold">{{ component.name }}</div>
+                  <div class="text-xs opacity-70">
+                    {{ component.desc }}
+                  </div>
+                </div>
+              </li>
+              <div class="p-1">
+                <Card v-for="re in component.recycle" :key="re.stream.name">
+                  <CardHeader class="p-4 pb-2">
+                    <CardTitle class="text-center">{{
+                      re.stream.name
+                    }}</CardTitle>
+                    <div class="flex flex-col items-start gap-2 relative">
+                      <img
+                        v-if="re.stream.container.image"
+                        :src="re.stream.container.image"
+                      />
+                      <img
+                        v-if="re.stream.container.image"
+                        :src="component.image_url"
+                        class="absolute w-15 h-15 rounded-xl"
+                        :style="{
+                          top: `${re.stream.container.image_entry_point.y}px`,
+                          left: `${re.stream.container.image_entry_point.x}px`,
+                        }"
+                      />
+                    </div>
+                  </CardHeader>
+                  <CardContent class="flex flex-col justify-center px-4 pb-3">
+                    <span class="text-xs line-clamp-3">{{
+                      re.stream.desc
+                    }}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </div>
+            <div class="divider m-1"></div>
+          </div>
+
+          <li v-else class="list-row">There are no components to show</li>
+        </ul>
+      </CollapsibleContent>
+    </Collapsible>
   </div>
 </template>
 
 <script setup lang="ts">
 const route = useRoute()
+
+const recyclingOpen = ref(true)
+
 const variantQuery = gql`
   query GetVariant($id: ID!) {
     getVariant(id: $id) {
@@ -64,6 +117,48 @@ const variantQuery = gql`
           name
           desc
           image_url
+        }
+      }
+    }
+  }
+`
+const variantRecycling = gql`
+  query GetVariantRecycling($id: ID!, $region: ID!) {
+    getVariant(id: $id) {
+      id
+      name
+      components {
+        nodes {
+          id
+          name
+          desc
+          image_url
+          recycle(region_id: $region) {
+            context {
+              key
+              desc
+            }
+            stream {
+              name
+              desc
+              container {
+                type
+                access
+                shape {
+                  width
+                  height
+                  depth
+                }
+                color
+                image
+                image_entry_point {
+                  x
+                  y
+                  side
+                }
+              }
+            }
+          }
         }
       }
     }
@@ -101,5 +196,22 @@ type VariantResult = {
 const { status, data } = await useLazyAsyncQuery<VariantResult>(
   variantQuery,
   vars,
+)
+
+const { result: recyclingResult, load: loadRecycling } = useLazyQuery(
+  variantRecycling,
+  {
+    id: route.params.id,
+    region: useRegionStore().selectedRegion,
+  },
+)
+watch(
+  recyclingOpen,
+  async (open) => {
+    if (open) {
+      await loadRecycling()
+    }
+  },
+  { immediate: true },
 )
 </script>
